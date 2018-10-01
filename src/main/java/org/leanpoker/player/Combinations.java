100dffdc061913a8cd9adae65c14108f6cfddc27
Milan Kiszely <milankiszely@gmail.com>
2018-10-01 11:40:36 +0200

    card contructor modified, card sorter created

--- src/main/java/org/leanpoker/player/Combinations.java
+++ src/main/java/org/leanpoker/player/Combinations.java
@@ -5,40 +5,62 @@ import java.util.Map;
 
 public class Combinations {
     public boolean hasPoker(Card[] cards){
-        Map<String, Integer> rankCounter = new HashMap<>();
-        for(String rank: getRanks(cards)){
+        Map<Integer, Integer> rankCounter = new HashMap<>();
+        for(Integer rank: getRanks(cards)){
             if (rankCounter.containsKey(rank))
                 rankCounter.put(rank, rankCounter.get(rank) + 1);
             else
                 rankCounter.put(rank, 1);
         }
-        for(String key: rankCounter.keySet())
-            if (rankCounter.get(key) == 4)
+        for(Integer rank: rankCounter.keySet())
+            if (rankCounter.get(rank) == 4)
                 return true;
         return false;
     }
 
     public boolean hasFull(Card[] cards){
-        Map<String, Integer> rankCounter = new HashMap<>();
-        for(String rank: getRanks(cards)){
+        Map<Integer, Integer> rankCounter = new HashMap<>();
+        for(Integer rank: getRanks(cards)){
             if (rankCounter.containsKey(rank))
                 rankCounter.put(rank, rankCounter.get(rank) + 1);
             else
                 rankCounter.put(rank, 1);
         }
-        String firstRank = "";
-        for(String key: rankCounter.keySet()){
+        Integer firstRank = 0;
+        for(Integer key: rankCounter.keySet()){
             if (rankCounter.get(key) >= 3)
-                firstRank = key;
-            else if (rankCounter.get(key) >= 2 && !key.equals(firstRank))
-                return true;
+                if (firstRank.equals(1))
+                    firstRank = key;
+                else
+                    return true;
+            else if (rankCounter.get(key) >= 2)
+                if (firstRank.equals(1))
+                    firstRank = key;
+                else
+                    return true;
 
         }
         return false;
     }
 
-    private String[] getRanks(Card[] cards){
-        String[] ranks = new String[cards.length];
+    public boolean hasStraight(Card[] cards){
+
+    }
+
+    private Card[] sortCards(Card[] cards){
+        for(int i = cards.length-1; i > 0; i--)
+            for(int j = 0; j < i; j++){
+                if(cards[j].getRank() > cards[j+1].getRank()) {
+                    Card tmp = cards[j];
+                    cards[j] = cards[j+1];
+                    cards[j+1] = tmp;
+                }
+            }
+        return cards;
+    }
+
+    private Integer[] getRanks(Card[] cards){
+        Integer[] ranks = new Integer[cards.length];
         int index = 0;
         for (Card card: cards){
             ranks[index] = card.getRank();
