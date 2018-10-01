8afecda6d51830c3000f75a8893692494b22269f
Mate Brunner <brunnerm7@gmail.com>
2018-10-01 13:55:51 +0200

    stack

--- src/main/java/org/leanpoker/player/Strategy.java
+++ src/main/java/org/leanpoker/player/Strategy.java
@@ -2,43 +2,43 @@ package org.leanpoker.player;
 
 public class Strategy {
 
-    public static int firstPhase(Card[] cardsArray, Integer buyIn) {
+    public static int firstPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
         return 0;
     }
 
-    public static int secondPhase(Card[] cardsArray, Integer buyIn) {
+    public static int secondPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
         if (Combinations.hasPoker(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasDrill(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasFlush(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasStraight(cardsArray))
-            return 10000;
+            return ourStack;
         return 0;
     }
 
-    public static int thirdPhase(Card[] cardsArray, Integer buyIn) {
+    public static int thirdPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
         if (Combinations.hasPoker(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasDrill(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasFlush(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasStraight(cardsArray))
-            return 10000;
+            return ourStack;
         return 0;
     }
 
-    public static int fourthPhase(Card[] cardsArray, Integer buyIn) {
+    public static int fourthPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
         if (Combinations.hasPoker(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasDrill(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasFlush(cardsArray))
-            return 10000;
+            return ourStack;
         if (Combinations.hasStraight(cardsArray))
-            return 10000;
+            return ourStack;
         return 0;
     }
 
