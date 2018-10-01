0a7aa13bc399bf624d4524476a70f6e77c40fe04
Unknown <regoczi.tamas01@gmail.com>
2018-10-01 10:36:59 +0200

    see if json works

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +11,25 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+
+        JsonObject obj = request.getAsJsonObject();
+
+        JsonElement el = obj.get("players");
+
+        JsonArray arr = el.getAsJsonArray();
+
+        JsonElement ourCards = null;
+
+        for(JsonElement element: arr){
+            if(((JsonObject) element).get("name").equals("KorDAO")){
+                ourCards = element;
+            }
+        }
+
+        System.out.println(ourCards);
+       
+
+        return 1;
     }
 
     public static void showdown(JsonElement game) {
