8f8fcaf847708179c0610a54be78bdc30e708e0c
Unknown <regoczi.tamas01@gmail.com>
2018-10-01 14:19:54 +0200

    is someone all in

--- src/main/java/org/leanpoker/player/Strategy.java
+++ src/main/java/org/leanpoker/player/Strategy.java
@@ -4,49 +4,51 @@ import java.util.List;
 
 public class Strategy {
 
-
     public static int firstPhase(Card[] cards, int currentBuyIn) {
 
         if(Combinations.hasPairInHand(cards) != 0){
             return currentBuyIn+25;
         }
+
+
+
         return 0;
 
     }
 
-    public static int secondPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
+    public static int secondPhase(Card[] cardsArray, Integer buyIn, Integer ourstack) {
         if (Combinations.hasPoker(cardsArray))
-            return ourStack;
+            return 10000;
+        if (Combinations.hasDrill(cardsArray))
+            return 10000;
         if (Combinations.hasFlush(cardsArray))
-            return ourStack;
-        if (Combinations.hasFull(cardsArray))
-            return ourStack;
+            return 10000;
         if (Combinations.hasStraight(cardsArray))
-            return ourStack;
+            return 10000;
         return 0;
     }
 
-    public static int thirdPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
+    public static int thirdPhase(Card[] cardsArray, Integer buyIn, Integer ourstack) {
         if (Combinations.hasPoker(cardsArray))
-            return ourStack;
+            return 10000;
+        if (Combinations.hasDrill(cardsArray))
+            return 10000;
         if (Combinations.hasFlush(cardsArray))
-            return ourStack;
-        if (Combinations.hasFull(cardsArray))
-            return ourStack;
+            return 10000;
         if (Combinations.hasStraight(cardsArray))
-            return ourStack;
+            return 10000;
         return 0;
     }
 
-    public static int fourthPhase(Card[] cardsArray, Integer buyIn, Integer ourStack) {
+    public static int fourthPhase(Card[] cardsArray, Integer buyIn, Integer ourstack) {
         if (Combinations.hasPoker(cardsArray))
-            return ourStack;
+            return 10000;
+        if (Combinations.hasDrill(cardsArray))
+            return 10000;
         if (Combinations.hasFlush(cardsArray))
-            return ourStack;
-        if (Combinations.hasFull(cardsArray))
-            return ourStack;
+            return 10000;
         if (Combinations.hasStraight(cardsArray))
-            return ourStack;
+            return 10000;
         return 0;
     }
 
